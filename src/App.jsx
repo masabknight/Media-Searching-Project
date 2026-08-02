6221971701308a013a910585e15a2a30da26b854
masab_knight <masabknight@gmail.com>
2026-08-02 17:20:34 +0500

    Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
-import React, { useEffect } from 'react'
-import { fetchGif, fetchPhotos, fetchVideos } from './api/mediaApi'
+import React from 'react'
+import SearchBar from './components/SearchBar'
 
 const App = () => {
-
   return (
     <div className='h-screen text-white w-full bg-gray-950'>
-
-      <button className='bg-green-400 px-4 py-2 m-5' onClick={async () => {
-        
-        const data = await fetchPhotos('cat');
-
-        console.log(data.results);
-
-      }}>Get Photos</button>
-
-      <button className='bg-green-400 px-4 py-2 m-5' onClick={async () => {
-
-        const data = await fetchVideos('cat');
-
-        console.log(data.videos);
-
-      }}>Get Videos</button>
-
-      <button className='bg-green-400 px-4 py-2 m-5' onClick={async () => {
-
-        const data = await fetchGif('cat');
-
-        console.log(data.data);
-
-      }}>Get Gifs</button>
-
+      <SearchBar/>
     </div>
   )
 }
